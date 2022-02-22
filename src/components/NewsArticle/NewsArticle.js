c045102e328a25032460d568c12c602ff20983b8
Joan Rasmussen <raz.joan@gmail.com>
2022-02-21 18:41:16 -0700

    Update NewsArticle with img and date

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -6,6 +6,8 @@ const NewsArticle = ({article}) => {
     <div>
       <h2>{article.title}</h2>
       <p>{article.byline}</p>
+      <p>{article.published_date}</p>
+      <img src={article.multimedia[2].url} alt={article.multimedia[2].caption}/>
     </div>
   )
 }
