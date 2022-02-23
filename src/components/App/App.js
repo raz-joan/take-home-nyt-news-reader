199ef9f631f7919f24ef39939f3c45d4c58617e4
Joan Rasmussen <raz.joan@gmail.com>
2022-02-23 14:54:57 -0700

    Add basic error handling to API fetch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,6 +8,7 @@ function App() {
 
   const [articles, setArticles] = useState([])
   const [section, setSection] = useState('Home')
+  const [error, setError] = useState('')
 
   useEffect(() => {
     fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=4bxWhQUEJsgqskrAo23GYzh6xYoBnPOE')
@@ -16,6 +17,7 @@ function App() {
         console.log('home response:', data)
         setArticles(data.results)
       })
+      .catch(err => setError('Uh Oh! There was an error connecting to the NYT database.'))
   }, [])
 
   const sendRequest = (category) => {
@@ -29,22 +31,27 @@ function App() {
         const title = category.slice(0, 1).toUpperCase() + category.slice(1)
         setSection(title)
       })
+      .catch(err => setError('Uh Oh! There was an error connecting to the NYT database.'))
   }
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <Link to='/' className='site-title'>
-          <h1>NYT NEWS READER</h1>
-        </Link>
-      </header>
-      <main>
-        <Routes>
-          <Route path='/' element={<Landing sendRequest={sendRequest} articles={articles} section={section}/>} />
-          <Route path='/articles/:index' element={<DetailedNewsArticle articles={articles}/>} />
-          <Route path='/*' element={<p className='error-url-path'>404: This page does not exist. Sorry!</p>}/>
-        </Routes>
-      </main>
+    <div>
+      {error ? <p className='error-url-path'>{ error }</p> :
+      <div className='App'>
+        <header className='App-header'>
+          <Link to='/' className='site-title'>
+            <h1>NYT NEWS READER</h1>
+          </Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path='/' element={<Landing sendRequest={sendRequest} articles={articles} section={section} />} />
+            <Route path='/articles/:index' element={<DetailedNewsArticle articles={articles} />} />
+            <Route path='/*' element={<p className='error-url-path'>404: This page does not exist. Sorry!</p>} />
+          </Routes>
+        </main>
+      </div>
+      }
     </div>
   );
 }
