f6c3230fba0c044be17277a4a8e2f4271da34d93
Joan Rasmussen <raz.joan@gmail.com>
2022-02-24 11:11:36 -0700

    Fix error handling on dynamic route :index

--- src/components/DetailedNewsArticle/DetailedNewsArticle.js
+++ src/components/DetailedNewsArticle/DetailedNewsArticle.js
@@ -4,28 +4,32 @@ import { useParams } from 'react-router-dom'
 
 const DetailedNewsArticle = ({ articles }) => {
 
-  const index = useParams().index
+  const index = Number(useParams().index)
   let article
   let date
 
-  if (articles.length) {
+  if (index && 0 <= index && index < articles.length) {
     article = articles[index]
     date = article.published_date.split('T')
-  } else { 
+  } else {
+    article = ''
     date = ''
   }
 
   return (
     <div className='detailed-container'>
-      {!date ? <p>Loading ...</p> :
-        <div className='detailed-news-article'>
-          <h2 className='detailed-article-title'>{article.title}</h2>
-          <img src={article.multimedia[1].url} alt={article.multimedia[1].caption} />
-          <p className='detailed-article-info'>{article.byline}</p>
-          <p className='detailed-article-info'>{date[0]}</p>
-          <p className='detailed-article-info'>{article.abstract}</p>
-          <a href={article.url} className='detailed-article-info'>Link to full article on the New York Times</a>
-        </div>}
+      {article ?
+        (!date ? <p>Loading ...</p> :
+          <div className='detailed-news-article'>
+            <h2 className='detailed-article-title'>{article.title}</h2>
+            <img src={article.multimedia[1].url} alt={article.multimedia[1].caption} />
+            <p className='detailed-article-info'>{article.byline}</p>
+            <p className='detailed-article-info'>{date[0]}</p>
+            <p className='detailed-article-info'>{article.abstract}</p>
+            <a href={article.url} className='detailed-article-info'>Link to full article on the New York Times</a>
+          </div>) :
+        <p className='error-url-path'>404: This page does not exist. Sorry!</p>
+      }
     </div>
   )
 }
